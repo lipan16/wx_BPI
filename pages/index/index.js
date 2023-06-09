5f30ed85091100dbf1814788bd86a99505f430ba
lipan <lipan16@lzu.edu.cn>
2023-06-09 21:00:31 +0800

    feat: 首页完善

--- pages/index/index.js
+++ pages/index/index.js
@@ -3,6 +3,42 @@ const APP = getApp()
 Page({
   data: {
     banners: [], // 轮播图
+    feats: [{
+        id: 'order',
+        name: '点菜',
+        icon: '../../images/default.png',
+        url: '',
+        isPage: true
+      },
+      {
+        id: 'banner',
+        name: 'banner',
+        icon: '../../images/default.png',
+        url: '/pages/banner/index',
+        isPage: true
+      },
+      {
+        id: 'workProgress',
+        name: '上班进度',
+        icon: '../../images/default.png',
+        url: '/pages/workProgress/index',
+        isPage: true
+      },
+      {
+        id: 'progress2',
+        name: 'test1',
+        icon: '../../images/default.png',
+        url: '',
+        isPage: false
+      },
+      {
+        id: 'progress3',
+        name: 'test2',
+        icon: '../../images/default.png',
+        url: '',
+        isPage: false
+      }
+    ] // 功能列表
   },
   // 监听页面加载
   onLoad() {
@@ -36,16 +72,16 @@ Page({
   },
   getHomeBanner() {
     const banners = [{
-        "id": 1,
-        "picUrl": "https://dcdn.it120.cc/2019/12/29/8396f65d-d615-46d8-b2e5-aa41820b9fe5.png",
+        'id': 1,
+        'picUrl': 'https://dcdn.it120.cc/2019/12/29/8396f65d-d615-46d8-b2e5-aa41820b9fe5.png'
       },
       {
-        "id": 2,
-        "picUrl": "https://dcdn.it120.cc/2019/12/29/daca65ee-4347-4792-a490-ccbac4b3c1d7.png",
+        'id': 2,
+        'picUrl': 'https://dcdn.it120.cc/2019/12/29/daca65ee-4347-4792-a490-ccbac4b3c1d7.png'
       },
       {
-        "id": 3,
-        "picUrl": "https://dcdn.it120.cc/2019/12/29/2e79921a-92b3-4d1d-8182-cb3d524be5fb.png",
+        'id': 3,
+        'picUrl': 'https://dcdn.it120.cc/2019/12/29/2e79921a-92b3-4d1d-8182-cb3d524be5fb.png'
       }
     ]
     this.setData({
@@ -54,21 +90,48 @@ Page({
   },
 
   // 事件处理函数
-  onClickSearch() {
+  onClickSearch() { // 点击搜索
     wx.showToast({
       title: '奋力开发中...',
       icon: 'none'
     })
   },
-  onClickBanner: function (e) {
+  onClickBanner: function (e) { // 点击banner
     const url = e.currentTarget.dataset.url
     url && wx.navigateTo({
       url
     })
   },
-  bindTapBanner() {
-    wx.navigateTo({
-      url: '/pages/banner/index'
-    })
-  },
+  onClickFeat(e) { // 点击功能
+    const item = this.data.feats.find(f => f.id === e.currentTarget.dataset.id)
+    console.log(item)
+    const {
+      url,
+      isPage
+    } = item
+    if (url) {
+      if (isPage) {
+        wx.navigateTo({
+          url
+        })
+      } else {
+        wx.showModal({
+          title: '系统提示',
+          content: '这是一个外部地址，是否跳转',
+          success(res) {
+            if (res.confirm) {
+              console.log('用户点击确定')
+            } else if (res.cancel) {
+              console.log('用户点击取消')
+            }
+          }
+        })
+      }
+    } else {
+      wx.showToast({
+        title: '奋力开发中...',
+        icon: 'none'
+      })
+    }
+  }
 })
