bfef6f72bdb28a74357320bb70186a60f9d60b1a
lipan <lipan16@lzu.edu.cn>
2023-06-10 22:04:27 +0800

    feat-我的页面增加获取个人信息接口

--- pages/my/index.js
+++ pages/my/index.js
@@ -2,49 +2,28 @@ const CONFIG = require('../../config.js')
 
 Page({
   data: {
+    version: '',
+    userInfo: {},
   },
-
   onLoad(options) {
-      this.setData({
-        version: CONFIG.version
-      })
-  },
-  onReady() {
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow() {
+    const userInfo = wx.getStorageSync('userInfo')
+    this.setData({
+      version: CONFIG.version,
+      userInfo
+    })
   },
 
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide() {
+  // 获取数据
+  getUserProfile() {
+    wx.getUserProfile({
+      desc: '用于完善用户信息', // 声明获取用户个人信息后的用途，后续会展示在弹窗中，请谨慎填写
+      lang: 'zh_CN',
+      success: res => {
+        this.setData({
+          userInfo: res.userInfo,
+        })
+        wx.setStorageSync('userInfo', res.userInfo)
+      }
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload() {
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh() {
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom() {
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage() {
-  }
 })
