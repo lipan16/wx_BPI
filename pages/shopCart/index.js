7979a41ab67a7da018e5c76e6243c871d3be383d
lipan <lipan16@lzu.edu.cn>
2023-06-10 22:05:05 +0800

    feat-购物车页面

--- pages/shopCart/index.js
+++ pages/shopCart/index.js
@@ -1,9 +1,4 @@
-// pages/shop-cart/index.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
 
   },
@@ -29,38 +24,9 @@ Page({
 
   },
 
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide() {
-
+  toIndexPage: function () {
+    wx.switchTab({
+      url: "/pages/index/index"
+    });
   },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload() {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh() {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom() {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage() {
-
-  }
 })
