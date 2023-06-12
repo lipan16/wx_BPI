3cb098f18b4da204ab9430a5935168c4a9e8f677
lipan <lipan16@lzu.edu.cn>
2023-06-13 00:21:49 +0800

    feat-通知消息详情页面

---
+++ pages/notice/detail.js
@@ -0,0 +1,24 @@
+const Api = require('apifm-wxapi');
+const CONFIG = require('../../config');
+
+Page({
+  data: {},
+  onLoad: function (options) {
+    Api.noticeDetail(options.id).then(res => {
+      if (res.code == CONFIG.apiSuccess) {
+        console.log('noticeDetail', res.data)
+        this.setData({
+          notice: res.data
+        })
+      }
+    })
+  },
+
+  onShareTimeline() {
+    return {
+      title: this.data.notice.title,
+      query: 'id=' + this.data.notice.id,
+      // imageUrl: wx.getStorageSync('share_pic')
+    }
+  },
+})
